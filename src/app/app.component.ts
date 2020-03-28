522bd6cbf985364b4e87f52aa269675d2131aba8
Dylan Lewis <christopher.hixson-lewis@okstate.edu>
2020-03-27 19:12:46 -0500

    Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../css/bootstrap.min.css', '../css/style.min.css', '../css/mdb.min.css']
+  
 })
 export class AppComponent {
   title = 'Alumni-Database';
