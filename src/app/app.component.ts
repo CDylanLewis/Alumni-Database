01c3c1a1fd39705ff1e8399ae551aa1470b1a010
CDylanLewis <christopher.hixson-lewis@okstate.edu>
2020-05-01 18:59:42 -0500

    Rework

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['../css/bootstrap.min.css', '../css/style.min.css', '../css/mdb.min.css', '../css/fontawesome/css/all.css'],
+
+})
+export class AppComponent {
+  title = 'OSU MSIS Alumni-Database';
+}
