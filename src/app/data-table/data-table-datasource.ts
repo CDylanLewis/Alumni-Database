9c5c005a981bff839f59751dbc25a819634fbfc2
CDylanLewis <christopher.hixson-lewis@okstate.edu>
2020-05-02 14:37:28 -0500

    Database Changes

--- src/app/data-table/data-table-datasource.ts
+++ src/app/data-table/data-table-datasource.ts
@@ -6,32 +6,17 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 
 // TODO: Replace this with your own data model type
 export interface DataTableItem {
-  name: string;
-  id: number;
+  FName: string;
+  LName: string;
+  Email: string;
+  Phone: string;
+  Company: string;
+  Position: string;
 }
 
 // TODO: replace this with real data from your application
 const EXAMPLE_DATA: DataTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+  { FName: 'Dylan', LName: 'Hixson-Lewis', Email: 'cdylanlewis1996@gmail.com', Phone: '9188141362', Company: 'OSU', Position: 'Tech' },
 ];
 
 /**
@@ -72,7 +57,7 @@ export class DataTableDataSource extends DataSource<DataTableItem> {
    *  Called when the table is being destroyed. Use this function, to clean up
    * any open connections or free any held resources that were set up during connect.
    */
-  disconnect() {}
+  disconnect() { }
 
   /**
    * Paginate the data (client-side). If you're using server-side pagination,
@@ -95,8 +80,8 @@ export class DataTableDataSource extends DataSource<DataTableItem> {
     return data.sort((a, b) => {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
+        case 'FName': return compare(a.FName, b.FName, isAsc);
+        case 'LName': return compare(+a.LName, +b.LName, isAsc);
         default: return 0;
       }
     });
